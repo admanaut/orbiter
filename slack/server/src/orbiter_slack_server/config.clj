9ff71c6e05726e3cd07acbd2563039ecb60dd105
Adrian Marina <adrianmarina.adrian@gmail.com>
2020-05-19 08:45:25 +0100

    add parseint for port number

--- slack/server/src/orbiter_slack_server/config.clj
+++ slack/server/src/orbiter_slack_server/config.clj
@@ -2,4 +2,4 @@
   (:use [orbiter-slack-server.validation :as v]))
 
 (defn get-config []
-  (v/map-v (fn [i] {:port i}) (isNotNil (get (System/getenv) "PORT") "Env var PORT is required")))
+  (v/map-v (fn [i] {:port (Integer/parseInt i)}) (isNotNil (get (System/getenv) "PORT") "Env var PORT is required")))
