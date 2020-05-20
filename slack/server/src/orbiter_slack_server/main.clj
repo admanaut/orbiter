e9ace510c826807f9b843472e2a4762ebf2ab5d4
Adrian Marina <adrianmarina.adrian@gmail.com>
2020-05-20 08:14:05 +0100

    print req

--- slack/server/src/orbiter_slack_server/main.clj
+++ slack/server/src/orbiter_slack_server/main.clj
@@ -4,21 +4,23 @@
         [orbiter-slack-server.config :as config]
         [orbiter-slack-server.validation :as v]))
 
-;; TODO: deploy, config, state, domain, modules
+;; TODO: config, state, domain, modules
 
 (defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "hello HTTP!"
-   })
+  (do
+    (println req)
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body "hello HTTP!"
+     }))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(defn -main [& args]
   (let [confv (config/get-config)
         conf (v/run-v confv)]
     (if (v/isSuccess confv)
-      (httpkit/run-server app {:port (:port conf)})
+      (do
+        (println "Server started on port " (:port conf))
+        (httpkit/run-server app {:port (:port conf)}))
       (do
         (println conf)
         (System/exit 1)))))
