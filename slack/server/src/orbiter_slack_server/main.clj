b95540a307ab42369bd56f8017303e39e2453ba6
Adrian Marina <adrianmarina.adrian@gmail.com>
2020-05-21 08:20:48 +0100

    add compojure for routing

--- slack/server/src/orbiter_slack_server/main.clj
+++ slack/server/src/orbiter_slack_server/main.clj
@@ -2,17 +2,28 @@
   (:gen-class)
   (:use [org.httpkit.server :as httpkit]
         [orbiter-slack-server.config :as config]
-        [orbiter-slack-server.validation :as v]))
+        [orbiter-slack-server.validation :as v]
+        [compojure.route :only [files not-found]]
+        [compojure.core :only [defroutes GET POST]]
+        [clojure.data.json :as json]
+        ))
 
 ;; TODO: config, state, domain, modules
 
-(defn app [req]
+(defn handle-help [req]
   (do
     (println req)
+    (println (slurp (:body req)))
     {:status 200
      :headers {"Content-Type" "text/html"}
-     :body "hello HTTP!"
+     :body "Welcome to Orbiter! here's what you can do next:"
      }))
+
+(defroutes app
+  (GET "/" [] handle-help)
+  (POST "/" [] handle-help)
+  (files "/static/")
+  (not-found "Page not found."))
 
 (defn -main [& args]
   (let [confv (config/get-config)
