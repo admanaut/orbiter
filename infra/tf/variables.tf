1ca623f42b96c0515be4e7316f58da76b3b570c6
Adrian Marina <adrianmarina.adrian@gmail.com>
2020-06-05 09:10:36 +0100

    remove tf output variables

--- infra/tf/variables.tf
+++ infra/tf/variables.tf
@@ -9,15 +9,3 @@ variable "linux_admin_username" {
 variable "linux_admin_password" {
   type = string
 }
-
-output "gcp_cluster_endpoint" {
-  value = "${google_container_cluster.primary_cluster.endpoint}"
-}
-
-output "gcp_ssh_commnad" {
-  value = "ssh ${var.linux_admin_urername}@${google_container_cluster.primary_cluster.endpoint}"
-}
-
-output "gcp_cluster_name" {
-  value = "${google_container_cluster.primary_cluster.name}"
-}
